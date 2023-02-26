c7732b0eae139a26ee1130b87ec120853d56ed98
Jabez <jabezli1990@gmail.com>
2023-02-25 16:08:47 -0800

    main function finished, now its able to add roles and generate a html file with all required function

--- Index.js
+++ Index.js
@@ -214,19 +214,24 @@ function employeeCards() {
     const htmlDivs = cardsArray.join("");
     //I have some preset html elements here then the htmlDivs will be insert into it.
     const htmlPage = `
-        <!doctype html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Team File Generator</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-            <link rel="stylesheet" href="/asset/style.css">
-        </head>
-        <body>
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Team File Generator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="/asset/style.css">
+      </head>
+      <body>
+        <header class="header">
+        <h1 class="header-h1">My Team</h1>
+        </header>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
         ${htmlDivs}
-        </body>
-        </html>`
+        </div>
+      </body>
+    </html>`
 
     fs.writeFile(`test.html`, htmlPage, (err) => {
         err ? console.log(err) : console.log('Webpage has been generated!')
